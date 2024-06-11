b63c32c14e5b44bae87b05c447934e60b0225d92
kirapablinski <pablomauna@hotmail.com>
2024-06-11 11:42:12 -0400

    fin parte1
    se pudo hacer el esqueleto de la pagina y unos cuantos estilos, no se pudo hacer que se conectara a firebase ni hacer los cambios de contraste y tamaño, ni el script

---
+++ asset/js/credenciales.js
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyC2jlgrGUbFIsqwuFDu4q3eLXSHo7UQyYM",
+    authDomain: "mi-proyecto-we.firebaseapp.com",
+    projectId: "mi-proyecto-we",
+    storageBucket: "mi-proyecto-we.appspot.com",
+    messagingSenderId: "497759599118",
+    appId: "1:497759599118:web:423f022285b1ab7cb61a5f"
+  };
